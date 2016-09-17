8ef9fa70cffc3a7a59daf2b7370b71cace19a8f5
Raphael Lima <raphael.lima@adtsys.com.br>
2016-09-17 13:19:24 -0300

    Fix bugs

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # This script generate all the infrastructure elements needed to run and deploy it on AWS.
 
-read -p "Insert Access Key: " AWS_ACCESS_KEY
-read -p "Insert Secret Key: " AWS_SECRET_KEY
+read -p "Insert Access Key: " AWS_ACCESS_KEY_ID
+read -p "Insert Secret Key: " AWS_SECRET_ACCESS_KEY
 
-export AWS_ACCESS_KEY=$AWS_ACCESS_KEY
-export AWS_SECRET_KEY=$AWS_SECRET_KEY
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 
 
 if ! type aws >> /dev/null ; then
@@ -13,5 +13,7 @@ if ! type aws >> /dev/null ; then
   sudo yum install wget unzip -y
   sudo wget --quiet https://s3.amazonaws.com/aws-cli/awscli-bundle.zip -O /tmp/awscli-bundle.zip && unzip -qo /tmp/awscli-bundle.zip -d /tmp/ && /tmp/awscli-bundle/install -i /usr/local/aws -b /usr/bin/aws
 fi
+
+aws ec2 create-key-pair --key-name docker-job --region us-east-1 --query 'KeyMaterial' --output text > docker-job.pem
 
 aws cloudformation create-stack --stack-name DockerJob --template-body file://application_stack_template.json --region us-east-1 --capabilities CAPABILITY_IAM
